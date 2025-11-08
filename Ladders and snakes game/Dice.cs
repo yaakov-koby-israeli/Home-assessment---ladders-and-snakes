69b981c81b1c5f38ea579ce00e6f0d834a7e3016
koby <kobyisr1@gmail.com>
2025-11-08 18:35:25 +0200

    Implement class dice

---
+++ Ladders and snakes game/Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game
+{
+    internal class Dice
+    {
+        private Random random = new Random();
+        public int RollTheDice()
+        {
+            // real return value between 1 and 6 
+            return random.Next(1, 7); 
+        }
+    }
+}
