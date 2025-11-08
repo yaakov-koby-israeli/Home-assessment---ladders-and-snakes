3b3f8041aaaf030b4bbda41907353698cdfe4c2f
koby <kobyisr1@gmail.com>
2025-11-08 22:13:45 +0200

    create empty cell class

---
+++ Ladders and snakes game/EmptyCell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game
+{
+    internal class EmptyCell: Cell
+    {
+        public EmptyCell(int index): base(index) { }
+
+        protected override int GetIndex()
+        {
+            return index;
+        }
+    }
+}
