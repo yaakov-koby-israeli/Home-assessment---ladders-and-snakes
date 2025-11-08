589b71021419ffe6b81a9e65350ec7dabef218e8
koby <kobyisr1@gmail.com>
2025-11-08 18:36:07 +0200

    Implement class Player

---
+++ Ladders and snakes game/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game
+{
+    internal class Player : IPlayer
+    {
+        private static int _initID = 1;
+
+        public int Id { get; private set; }
+
+        public int Position { get; set; } = 0;
+
+        public bool HasWon { get; set; } = false;
+
+        public Player()
+        {
+            Id = _initID++;
+        }
+    }
+}
