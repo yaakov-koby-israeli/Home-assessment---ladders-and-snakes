3ebf6a7c42513af9b18f5beba4718bf53b92afcd
koby <kobyisr1@gmail.com>
2025-11-12 11:59:39 +0200

    implement ladder link class

---
+++ Ladders and snakes game/Core/LadderLink.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladders_and_snakes_game.Players;
+
+namespace Ladders_and_snakes_game.Core
+{
+    internal class LadderLink
+    {
+        private readonly TopOrBottomCell _bottomCell;
+        private readonly TopOrBottomCell _topCell;
+        public LadderLink(TopOrBottomCell bottomIndex, TopOrBottomCell topIndex)
+        {
+            _bottomCell = bottomIndex;
+            _topCell = topIndex;
+        }
+        public TopOrBottomCell GetBottomCell()
+        {
+            return _bottomCell;
+        }
+        public TopOrBottomCell GetTopCell()
+        {
+            return _topCell;
+        }
+
+        public void MovePlayerUp(IPlayer player)
+        {
+            player.Position = _topCell.GetIndex();
+        }
+    }
+}
